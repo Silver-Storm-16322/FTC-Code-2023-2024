e3540ef5b8d08c1a9994e470c616094b5c236078
Lolbit757575 <cosmicaura3@gmail.com>
2023-09-28 20:23:58 -0500

    Adjusted field centric mecanum

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,5 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-  public static final String APP_BUILD_TIME = "2023-09-23T10:34:01.505-0500";
+  public static final String APP_BUILD_TIME = "2023-09-26T19:49:48.566-0500";
 }
